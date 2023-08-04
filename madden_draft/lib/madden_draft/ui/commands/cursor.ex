8317fd5c67be63d4b0242f2458520aa7ba001ea6
heitoranjos15 <heitoranjos96@gmail.com>
2023-08-04 22:35:06 +0100

    fix: cursor label focused not working properly

--- madden_draft/lib/madden_draft/ui/commands/cursor.ex
+++ madden_draft/lib/madden_draft/ui/commands/cursor.ex
@@ -9,17 +9,25 @@ defmodule MaddenDraft.View.Command.Cursor do
 
   defp change_position(model, new_position) do
     limit = tab_field_limit(model)
+    begin = 0
 
-    # IO.puts(new_position)
-
-    cursor_pos =
+    cursor_new_position =
       cond do
-        limit < new_position -> 0
-        new_position < 0 -> limit
+        limit < new_position -> begin
+        new_position < begin -> limit
         true -> new_position
       end
 
-    %{model | cursor: %{x: cursor_pos, label_focus: label_focused(model)}}
+    %{
+      model
+      | cursor: %{x: cursor_new_position, label_focus: label_focused(model, cursor_new_position)}
+    }
+  end
+
+  def label_focused(model, new_position) when is_number(new_position) do
+    %{current_tab: current_tab} = model
+
+    Enum.at(current_tab.fields(model), new_position)
   end
 
   def label_focused(model, tab_selected) do
